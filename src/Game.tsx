da55a262eb4c13670b3ec183a6cb08853d7f4673
Hector Romero <hectorromerodev@gmail.com>
2023-03-01 14:41:22 -0800

    add tailwindcss config

---
+++ src/Game.tsx
@@ -0,0 +1,13 @@
+function Game() {
+  return (
+  <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-green-600 to-green-700">
+    <h1 className="text-center text-5xl mb-4 font-display text-white">Tic tac toe game</h1>
+    <>
+      <div>Board goes here</div>
+      <div>Game score goes here</div>
+    </>
+  </div>
+  )
+}
+
+export default Game
